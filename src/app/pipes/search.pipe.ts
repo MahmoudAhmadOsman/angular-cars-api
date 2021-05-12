844c2e96836dd4aabdf1a0145be24d7665fb1e38
Mahmoud Osman <mahmoudosman.som@gmail.com>
2021-05-12 00:42:40 -0500

    added search filter

--- src/app/pipes/search.pipe.ts
+++ src/app/pipes/search.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class SearchPipe implements PipeTransform {
 
   transform(value: any, ...args: any): any {
-
+    // console.log(args)
     if (!args) {
       return value;
     }
